a881f63f2b6c114d824e5d657a3a7c7632981fad
Eldar Guseynov <demort321@gmail.com>
2022-11-13 15:59:26 +0300

    Task is done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,18 +1,61 @@
-function parseCount(value){
-    let parsedValue = Number.parseInt(value);
-    if (isNaN(parsedValue) == true){
-      throw new Error("Невалидное значение");
-    }
-    else{
-    return parsedValue;
-    }
+//_______________Task 1_____________
+
+
+function parseCount(value) {
+  let parsedValue = Number.parseInt(value);
+  if (isNaN(parsedValue) == true) {
+    throw new Error("Невалидное значение");
   }
-  
-function validateCount(value){
-  try{
+  else {
+    return parsedValue;
+  }
+}
+
+function validateCount(value) {
+  try {
     return parseCount(value)
   }
-  catch(error){
+  catch (error) {
     return error;
+  }
+}
+
+
+// ____________Task 2______________
+
+class Triangle {
+  constructor(firstLine, secondLine, thirdLine) {
+    this.firstLine = firstLine,
+      this.secondLine = secondLine,
+      this.thirdLine = thirdLine
+
+    if (firstLine + secondLine < thirdLine || secondLine + thirdLine < firstLine || firstLine + thirdLine < secondLine) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  getPerimeter() {
+    return this.firstLine + this.secondLine + this.thirdLine;
+  }
+
+  getArea() {
+    let p = this.getPerimeter() * 0.5;
+    let square = Math.sqrt(p * (p - this.firstLine) * (p - this.secondLine) * (p - this.thirdLine));
+    return Number(square.toFixed(3));
+  }
+}
+function getTriangle(firstLine, secondLine, thirdLine) {
+  try {
+    return new Triangle(firstLine, secondLine, thirdLine)
+  }
+  catch (error) {
+    return {
+      getArea() {
+        return "Ошибка! Треугольник не существует"
+      },
+      getPerimeter() {
+        return "Ошибка! Треугольник не существует"
+      }
+    }
   }
 }
